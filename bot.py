c3bd1894c153cd19f55abcd80fa03de19ae9f01a
yunnieyunnie <115853279+yunnieyunnie@users.noreply.github.com>
2022-10-22 13:33:41 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,5 @@
+import telepot
+
+bot = telepot.Bot("5639985541:AAFl7ExhxXM3lYHMqiPSWNbDrm5OafSNKNQ")
+
+bot.sendPhoto(chat_id="925554964", photo=("https://www.allaboutgardening.com/wp-content/uploads/2022/04/Pink-Flowers-in-Garden-1200x667.jpg"))
